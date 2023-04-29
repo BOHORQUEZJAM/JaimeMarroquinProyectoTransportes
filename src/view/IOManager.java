15889267a7162152efdc31b76f694def42a980f6
BOHORQUEZJAM <hetarquio@gmail.com>
2023-04-29 09:49:16 -0500

    se modifivo la clase IOManager  y ya funcionna los  paneles

--- src/view/IOManager.java
+++ src/view/IOManager.java
@@ -1,18 +1,36 @@
 package view;
 
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 
-public class IOManager extends JFrame {
+public class IOManager extends JFrame implements ActionListener {
 
 	private JPanel panelUno;
 	private JPanel panelVenta;
 	private JScrollPane scrollPane;
+	private JButton btonUno;
+	private JButton btonDos;
+	private JButton btonTres;
+	private JButton btonCuatro;
+	private JButton btonCinco;
+	private JButton btonSeis;
+	private ImageIcon iconoUno;
+	private ImageIcon iconoDos;
+	private ImageIcon iconoTres;
+	private ImageIcon iconoCuatro;
+	private ImageIcon iconoCinco;
+	private ImageIcon iconoSeis;
 
 	public IOManager() {
 		setSize(800, 600);
@@ -22,15 +40,55 @@ public class IOManager extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		scrollPane = new JScrollPane();
-		panelUno = new PanelPrincipal();
+		panelUno = new JPanel();
 		panelUno.setBounds(200, 70, 400, 400);
-		panelUno.setVisible(false);
+		panelUno.setLayout(null);
+		panelUno.setBorder(new LineBorder(Color.decode("#0C5172"),40));
 		add(panelUno);
+		iconoUno = new ImageIcon("src/imagenes/terminal-de-punto-de-venta.png");
+		iconoDos = new ImageIcon("src/imagenes/ruta.png");
+		iconoTres= new ImageIcon("src/imagenes/comedor.png");
+		iconoCuatro = new ImageIcon("src/imagenes/cama-del-hotel.png");
+		iconoCinco=  new ImageIcon("src/imagenes/informacion.png");
+		btonUno = new JButton(iconoUno);
+		btonUno.setBorder(null);
+		btonUno.setActionCommand("hola");
+		btonUno.addActionListener(this);
+		btonUno.setBounds(60, 60, 66, 66);
+		panelUno.add(btonUno);
+		btonDos = new JButton(iconoDos);
+		btonDos.setBorder(null);
+		btonDos.setBounds(160,60, 66, 66);
+		panelUno.add(btonDos);
+		btonTres = new JButton(iconoTres);
+		btonTres.setBounds(260,60,66,66);
+		panelUno.add(btonTres);
+		btonCuatro = new JButton(iconoCuatro);
+		btonCuatro.setBounds(60,140,66,66);
+		panelUno.add(btonCuatro);
+		btonCinco = new JButton(iconoCinco);
+		btonCinco.setBounds(160,140,66,66);
+		panelUno.add(btonCinco);
+		btonSeis = new JButton();
+		btonSeis.setBounds(260,140, 66,66);
+		panelUno.add(btonSeis);
 		panelVenta= new PanelVentas(scrollPane);
 		panelVenta.setBounds(180,20,400,500);
-		
+		panelVenta.setVisible(false);
 		add(panelVenta);
-		
+
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals(btonUno.getActionCommand())) {
+			panelUno.setVisible(false);
+			panelVenta.setVisible(true);
+		} else {
+
+
+		}
+
+	}
 }
--- src/view/PanelPrincipal.java
+++ src/view/PanelPrincipal.java
@@ -1,76 +1,78 @@
-package view;
-
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
-
-public class PanelPrincipal extends JPanel implements ActionListener {
-	private JButton btonUno;
-	private JButton btonDos;
-	private JButton btonTres;
-	private JButton btonCuatro;
-	private JButton btonCinco;
-	private JButton btonSeis;
-	private ImageIcon iconoUno;
-	private ImageIcon iconoDos;
-	private ImageIcon iconoTres;
-	private ImageIcon iconoCuatro;
-	private ImageIcon iconoCinco;
-	private ImageIcon iconoSeis;
-	public PanelPrincipal() {
-		
-		setBorder(new LineBorder(Color.decode("#0C5172"),40));
-		setLayout(null);
-		iconoUno = new ImageIcon("src/imagenes/terminal-de-punto-de-venta.png");
-		iconoDos = new ImageIcon("src/imagenes/ruta.png");
-		iconoTres= new ImageIcon("src/imagenes/comedor.png");
-		iconoCuatro = new ImageIcon("src/imagenes/cama-del-hotel.png");
-		iconoCinco=  new ImageIcon("src/imagenes/informacion.png");
-		btonUno = new JButton(iconoUno);
-		btonUno.setBorder(null);
-		btonUno.setActionCommand("hola");
-		btonUno.addActionListener(this);
-		btonUno.setBounds(60, 60, 66, 66);
-		add(btonUno);
-		btonDos = new JButton(iconoDos);
-		btonDos.setBorder(null);
-		btonDos.setBounds(160,60, 66, 66);
-		add(btonDos);
-		btonTres = new JButton(iconoTres);
-		btonTres.setBounds(260,60,66,66);
-		add(btonTres);
-		btonCuatro = new JButton(iconoCuatro);
-		btonCuatro.setBounds(60,140,66,66);
-		add(btonCuatro);
-		btonCinco = new JButton(iconoCinco);
-		btonCinco.setBounds(160,140,66,66);
-		add(btonCinco);
-		btonSeis = new JButton();
-		btonSeis.setBounds(260,140, 66,66);
-		add(btonSeis);
-		
-	}
-	
-
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-	}
-
-}
+//package view;
+//
+//import java.awt.CardLayout;
+//import java.awt.Color;
+//import java.awt.Component;
+//import java.awt.Graphics;
+//import java.awt.Insets;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.io.File;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.BorderFactory;
+//import javax.swing.ImageIcon;
+//import javax.swing.JButton;
+//import javax.swing.JOptionPane;
+//import javax.swing.JPanel;
+//import javax.swing.border.Border;
+//import javax.swing.border.LineBorder;
+//
+//public class PanelPrincipal extends JPanel implements ActionListener {
+//	private JButton btonUno;
+//	private JButton btonDos;
+//	private JButton btonTres;
+//	private JButton btonCuatro;
+//	private JButton btonCinco;
+//	private JButton btonSeis;
+//	private ImageIcon iconoUno;
+//	private ImageIcon iconoDos;
+//	private ImageIcon iconoTres;
+//	private ImageIcon iconoCuatro;
+//	private ImageIcon iconoCinco;
+//	private ImageIcon iconoSeis;
+//	
+//		iconoUno = new ImageIcon("src/imagenes/terminal-de-punto-de-venta.png");
+//		iconoDos = new ImageIcon("src/imagenes/ruta.png");
+//		iconoTres= new ImageIcon("src/imagenes/comedor.png");
+//		iconoCuatro = new ImageIcon("src/imagenes/cama-del-hotel.png");
+//		iconoCinco=  new ImageIcon("src/imagenes/informacion.png");
+//		btonUno = new JButton(iconoUno);
+//		btonUno.setBorder(null);
+//		btonUno.setActionCommand("hola");
+//		btonUno.addActionListener(this);
+//		btonUno.setBounds(60, 60, 66, 66);
+//		add(btonUno);
+//		btonDos = new JButton(iconoDos);
+//		btonDos.setBorder(null);
+//		btonDos.setBounds(160,60, 66, 66);
+//		add(btonDos);
+//		btonTres = new JButton(iconoTres);
+//		btonTres.setBounds(260,60,66,66);
+//		add(btonTres);
+//		btonCuatro = new JButton(iconoCuatro);
+//		btonCuatro.setBounds(60,140,66,66);
+//		add(btonCuatro);
+//		btonCinco = new JButton(iconoCinco);
+//		btonCinco.setBounds(160,140,66,66);
+//		add(btonCinco);
+//		btonSeis = new JButton();
+//		btonSeis.setBounds(260,140, 66,66);
+//		add(btonSeis);
+//		
+//	}
+//	
+//
+//
+//
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		if (e.getActionCommand().equals(btonUno.getActionCommand())) {
+//			PanelVentas obVentas = new PanelVentas(null);
+//			obVentas.setVisible(true);
+//		} else {
+//
+//		}
+//	}
+//
+//}
